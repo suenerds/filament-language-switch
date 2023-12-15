6fa038f66169de9c56ad819f03677a4224d23651
Thorger Suenert <thorger.suenert@suenert.de>
2023-12-15 15:10:43 +0100

    Fix formatting

--- resources/views/language-switch.blade.php
+++ resources/views/language-switch.blade.php
@@ -9,21 +9,24 @@
     $placement = match (true) {
         $outsidePanelsPlacement === 'top-center' && $isFlagsOnly => 'bottom',
         $outsidePanelsPlacement === 'bottom-center' && $isFlagsOnly => 'top',
-        !$isVisibleOutsidePanels && $isFlagsOnly => 'bottom',
+        ! $isVisibleOutsidePanels && $isFlagsOnly => 'bottom',
         default => 'bottom-end',
     };
     $maxHeight = $languageSwitch->getMaxHeight();
 @endphp
+
 <div>
     @if ($isVisibleOutsidePanels)
-        <div @class([
-            'fls-display-on fixed w-full flex p-4 z-50',
-            'top-0' => str_contains($outsidePanelsPlacement, 'top'),
-            'bottom-0' => str_contains($outsidePanelsPlacement, 'bottom'),
-            'justify-start' => str_contains($outsidePanelsPlacement, 'left'),
-            'justify-end' => str_contains($outsidePanelsPlacement, 'right'),
-            'justify-center' => str_contains($outsidePanelsPlacement, 'center'),
-        ])>
+        <div
+            @class([
+                'fls-display-on fixed z-50 flex w-full p-4',
+                'top-0' => str_contains($outsidePanelsPlacement, 'top'),
+                'bottom-0' => str_contains($outsidePanelsPlacement, 'bottom'),
+                'justify-start' => str_contains($outsidePanelsPlacement, 'left'),
+                'justify-end' => str_contains($outsidePanelsPlacement, 'right'),
+                'justify-center' => str_contains($outsidePanelsPlacement, 'center'),
+            ])
+        >
             <div class="rounded-lg bg-gray-50 dark:bg-gray-950">
                 @include('filament-language-switch::switch')
             </div>
--- resources/views/switch.blade.php
+++ resources/views/switch.blade.php
@@ -1,58 +1,85 @@
-<x-filament::dropdown teleport :placement="$placement" :width="$isFlagsOnly ? 'flags-only' : 'fls-dropdown-width'" :max-height="$maxHeight" class="fi-dropdown fi-user-menu">
+<x-filament::dropdown
+    teleport
+    :placement="$placement"
+    :width="$isFlagsOnly ? 'flags-only' : 'fls-dropdown-width'"
+    :max-height="$maxHeight"
+    class="fi-dropdown fi-user-menu"
+>
     <x-slot name="trigger">
-        <div @class([
-            'flex items-center justify-center w-9 h-9 language-switch-trigger text-primary-600 bg-primary-500/10',
-            'rounded-full' => $isCircular,
-            'rounded-lg' => !$isCircular,
-            'p-1 ring-2 ring-inset ring-gray-200 hover:ring-gray-300 dark:ring-gray-500 hover:dark:ring-gray-400' =>
-                $isFlagsOnly || $hasFlags,
-        ])
+        <div
+            @class([
+                'language-switch-trigger flex h-9 w-9 items-center justify-center bg-primary-500/10 text-primary-600',
+                'rounded-full' => $isCircular,
+                'rounded-lg' => ! $isCircular,
+                'p-1 ring-2 ring-inset ring-gray-200 hover:ring-gray-300 dark:ring-gray-500 hover:dark:ring-gray-400' => $isFlagsOnly || $hasFlags,
+            ])
             x-tooltip="{
                 content: @js($languageSwitch->getLabel(app()->getLocale())),
                 theme: $store.theme,
-                placement: 'bottom'
-            }">
+                placement: 'bottom',
+            }"
+        >
             @if ($isFlagsOnly || $hasFlags)
-                <x-filament-language-switch::flag :src="$languageSwitch->getFlag(app()->getLocale())" :circular="$isCircular" :alt="$languageSwitch->getLabel(app()->getLocale())"
-                    :switch="true" />
+                <x-filament-language-switch::flag
+                    :src="$languageSwitch->getFlag(app()->getLocale())"
+                    :circular="$isCircular"
+                    :alt="$languageSwitch->getLabel(app()->getLocale())"
+                    :switch="true"
+                />
             @else
-                <span class="font-semibold text-md">{{ $languageSwitch->getCharAvatar(app()->getLocale()) }}</span>
+                <span class="text-md font-semibold">
+                    {{ $languageSwitch->getCharAvatar(app()->getLocale()) }}
+                </span>
             @endif
         </div>
     </x-slot>
 
     <x-filament::dropdown.list @class(['!border-t-0 space-y-1 !p-2.5'])>
         @foreach ($locales as $locale)
-            @if (!app()->isLocale($locale))
-                <button type="button" wire:click="changeLocale('{{ $locale }}')"
+            @if (! app()->isLocale($locale))
+                <button
+                    type="button"
+                    wire:click="changeLocale('{{ $locale }}')"
                     @if ($isFlagsOnly) x-tooltip="{
                         content: @js($languageSwitch->getLabel($locale)),
                         theme: $store.theme,
-                        placement: 'right'
+                        placement: 'right',
                     }" @endif
                     @class([
-                        'flex items-center w-full transition-colors duration-75 rounded-md outline-none fi-dropdown-list-item whitespace-nowrap disabled:pointer-events-none disabled:opacity-70 fi-dropdown-list-item-color-gray hover:bg-gray-950/5 focus:bg-gray-950/5 dark:hover:bg-white/5 dark:focus:bg-white/5',
+                        'fi-dropdown-list-item fi-dropdown-list-item-color-gray flex w-full items-center whitespace-nowrap rounded-md outline-none transition-colors duration-75 hover:bg-gray-950/5 focus:bg-gray-950/5 disabled:pointer-events-none disabled:opacity-70 dark:hover:bg-white/5 dark:focus:bg-white/5',
                         'justify-center px-2 py-0.5' => $isFlagsOnly,
-                        'justify-start space-x-2 rtl:space-x-reverse p-1' => !$isFlagsOnly,
-                    ])>
-
+                        'justify-start space-x-2 p-1 rtl:space-x-reverse' => ! $isFlagsOnly,
+                    ])
+                >
                     @if ($isFlagsOnly)
-                        <x-filament-language-switch::flag :src="$languageSwitch->getFlag($locale)" :circular="$isCircular" :alt="$languageSwitch->getLabel($locale)"
-                            class="w-7 h-7" />
+                        <x-filament-language-switch::flag
+                            :src="$languageSwitch->getFlag($locale)"
+                            :circular="$isCircular"
+                            :alt="$languageSwitch->getLabel($locale)"
+                            class="h-7 w-7"
+                        />
                     @else
                         @if ($hasFlags)
-                            <x-filament-language-switch::flag :src="$languageSwitch->getFlag($locale)" :circular="$isCircular" :alt="$languageSwitch->getLabel($locale)"
-                                class="p-0.5 w-7 h-7" />
+                            <x-filament-language-switch::flag
+                                :src="$languageSwitch->getFlag($locale)"
+                                :circular="$isCircular"
+                                :alt="$languageSwitch->getLabel($locale)"
+                                class="h-7 w-7 p-0.5"
+                            />
                         @else
-                            <span @class([
-                                'flex items-center justify-center flex-shrink-0 w-7 h-7 p-2 text-xs font-semibold group-hover:bg-white group-hover:text-primary-600 group-hover:border group-hover:border-primary-500/10 group-focus:text-white bg-primary-500/10 text-primary-600',
-                                'rounded-full' => $isCircular,
-                                'rounded-lg' => !$isCircular,
-                            ])>
+                            <span
+                                @class([
+                                    'flex h-7 w-7 flex-shrink-0 items-center justify-center bg-primary-500/10 p-2 text-xs font-semibold text-primary-600 group-hover:border group-hover:border-primary-500/10 group-hover:bg-white group-hover:text-primary-600 group-focus:text-white',
+                                    'rounded-full' => $isCircular,
+                                    'rounded-lg' => ! $isCircular,
+                                ])
+                            >
                                 {{ $languageSwitch->getCharAvatar($locale) }}
                             </span>
                         @endif
-                        <span class="text-sm font-medium text-gray-600 hover:bg-transparent dark:text-gray-200">
+                        <span
+                            class="text-sm font-medium text-gray-600 hover:bg-transparent dark:text-gray-200"
+                        >
                             {{ $languageSwitch->getLabel($locale) }}
                         </span>
                     @endif
